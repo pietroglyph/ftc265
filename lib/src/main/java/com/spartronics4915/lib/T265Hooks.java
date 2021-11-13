e8ee9126e051a60e6dcd0f82eeea20b12816ba46
ryleu <riley.j.mclain@gmail.com>
2021-11-13 16:45:21 -0500

    Disable hooks for now because they were causing the crash

--- lib/src/main/java/com/spartronics4915/lib/T265Hooks.java
+++ lib/src/main/java/com/spartronics4915/lib/T265Hooks.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.util.Log;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.ftccommon.FtcEventLoop;
-import org.firstinspires.ftc.ftccommon.external.OnCreateEventLoop;
-import org.firstinspires.ftc.ftccommon.external.OnDestroy;
 
 public class T265Hooks {
     private static final String kLogTag = "ftc265";
     public static T265Camera slamera;
 
-    @OnCreateEventLoop
+    // @OnCreateEventLoop
     public static void attachEventLoop(Context appContext, FtcEventLoop eventLoop) {
         Log.d(kLogTag, "Creating T265Camera");
         slamera = new T265Camera(new Pose2d(), 0.8, appContext);
@@ -19,7 +17,7 @@ public class T265Hooks {
         Log.d(kLogTag, "T265Camera created");
     }
 
-    @OnDestroy
+    // @OnDestroy
     public static void destroySlamera(Context appContext) {
         Log.d(kLogTag, "Destroying T265Camera");
         slamera.stop();
