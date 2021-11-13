c80b4301ed003e14ac6a36c6f9a47e8847db91bd
ryleu <riley.j.mclain@gmail.com>
2021-11-13 16:47:55 -0500

    Fix transformation code and throw an error on the other math because it hurts my head

--- lib/src/main/java/com/spartronics4915/lib/PoseMath.java
+++ lib/src/main/java/com/spartronics4915/lib/PoseMath.java
@@ -6,6 +6,8 @@ public class PoseMath {
     /**
      * Transforms a Pose2d by another Pose2d.
      *
+     * <a href="https://www.desmos.com/calculator/0x7xoeiudh">A simulator for this behavior can be found here.</a>
+     *
      * @param t The first pose.
      * @param b The pose to transform the first pose by.
      * @return The result of the transformation.
@@ -14,33 +16,22 @@ public class PoseMath {
         double cos_t = Math.cos(t.getHeading());
         double sin_t = Math.sin(t.getHeading());
 
-        double cos_b = Math.cos(b.getHeading());
-        double sin_b = Math.sin(b.getHeading());
-
         return new Pose2d(
                 t.getX() + (b.getX() * cos_t - b.getY() * sin_t),
-                t.getY() + (b.getX() * sin_t - b.getY() * cos_t),
-                Math.atan2(cos_t * sin_b + sin_t * cos_b, cos_t * cos_b - sin_t * sin_b));
+                t.getY() + (b.getX() * sin_t + b.getY() * cos_t),
+                t.getHeading() + b.getHeading());
     }
 
     /**
      * Returns the other pose relative to the current pose.
      *
-     * @param initial The initial pose.
+     * @param initial The initial pose. (T)
      * @param other The pose that is the origin of the new coordinate frame that the initial pose
-     *     will be converted into.
+     *     will be converted into. (R)
      * @return The initial pose relative to the new origin pose.
      */
     public static Pose2d relativeTo(Pose2d initial, Pose2d other) {
-        double other_sin = Math.sin(-other.getHeading());
-        double other_cos = Math.cos(-other.getHeading());
-
-        double x_diff = other.getX() - initial.getX();
-        double y_diff = other.getY() - initial.getY();
-
-        return new Pose2d(
-                x_diff * other_cos - y_diff * other_sin,
-                y_diff * other_sin + y_diff * other_cos,
-                other.getHeading() - initial.getHeading());
+        // TODO: implement this math
+        throw new UnsupportedOperationException("setPose is not implemented");
     }
 }
